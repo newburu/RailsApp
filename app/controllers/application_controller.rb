535adacc3a9fbbe5f38356c1af45516ddd22a4d0
junich <GitHub>
2021-11-09 20:53:23 +0900

    login-view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
     protected
 
         def configure_permitted_parameters
-            devise_parameter_sanitizer.permit(:sign_up,keys: [:name, :gender,:weight,:height,:exercise])
+            devise_parameter_sanitizer.permit(:sign_up,keys: [:name, :gender, :weight, :height, :exercise])
         end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
-  enum gender: {man: 0,woman: 1}
-  enum exercise: {everytime: 0,Sometimes: 1,donot: 2}
-  validates :name, presence: true,length: {maximum:6}
-  # with_options do
-  #   validates :gender,presence: true
-  #   validates :weight,presence: true
-  #   validates :height,presence: true
-  #   validates :exercise,presence: true
-  #   end
+  enum gender: {man: 0, woman: 1}
+  enum exercise: {everytime: 0, Sometimes: 1, donot: 2}
 
   with_options presence: true do
+    validates :name, presence: true, length: {maximum:6}
     validates :gender
     validates :weight
     validates :height
