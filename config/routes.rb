6f8ecc9b2230e1f5ea3ab40a1767841cfaa28f78
junich <GitHub>
2021-11-10 12:46:34 +0900

    confirm-view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'homes#index'
+
+  devise_scope :user do
+    post 'users/sign_up/next', to: 'users/registrations#next'
+    post 'users/sign_up/confirm', to: 'users/registrations#confirm'
+  end
 end
