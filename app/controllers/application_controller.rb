c915f7fd247ac712c820a050452a8336dd80d3dc
junich <GitHub>
2021-11-16 20:13:41 +0900

    days-index.html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
+    helper_method :goalweight,
 
     protected
+        #ログインしているユーザーの目標体重を計算するメソッド
+        def goalweight
+            (@user.weight*0.95).round(2)
+        end
 
         def configure_permitted_parameters
             devise_parameter_sanitizer.permit(:sign_up,keys: [:name, :gender, :weight, :height, :exercise])
