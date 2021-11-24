b3765c47770f49cff3ab060828e66a9e7644746d
junich <GitHub>
2021-11-24 17:51:49 +0900

    detail

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'users/show'
   root 'homes#index'
 
-  resources :energys,only:[:new,:create,:index] do
+  resources :energys, only:[:new,:create,:index] do
     collection do
       get :weight
       get :record
