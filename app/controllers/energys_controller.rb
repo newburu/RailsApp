5710a389f295d8472f728546ed185c44c7a9b3d0
junich <GitHub>
2021-12-16 14:09:41 +0900

    energy-controller-index

--- app/controllers/energys_controller.rb
+++ app/controllers/energys_controller.rb
@@ -3,8 +3,52 @@ class EnergysController < ApplicationController
   def index
     @user = current_user
     @today = Date.today#今日の日付 
-    @energys = current_user.energys.where(date: @today)#ログインしているユーザーの今日の日付を全件取得してインスタンス変数に代入
-    # @protein = @energys.sum
+    energys = current_user.energys.where(date: @today)#ログインしているユーザーの今日の日付を全件取得（配列）
+    protein_amounts = energys.pluck(:protein).sum#配列で取得した値をカラムごとの配列に直してsumメソッドでたす
+    sugar_amounts = energys.pluck(:sugar).sum
+    kcal_amounts = energys.pluck(:kcal).sum
+    if @user.gender == "man"
+      if @user.exercise == "everytime"
+        (@user.weight*1.2).round(0)-protein_amounts
+      else
+        (@user.weight*1).round(0)-protein_amounts
+      end
+      if @user.exercise == "everytime"
+        2600-kcal_amounts
+      elsif @user.exercise == "Sometimes"
+        2400-kcal_amounts
+      else
+        2200-kcal_amounts
+      end
+      
+      if @user.exercise == "everytime"
+        390-sugar_amounts
+      elsif @user.exercise == "Sometimes"
+        360-sugar_amounts
+      else
+        330-sugar_amounts
+      end
+    else
+      if @user.exercise == "everytime"
+        (@user.weight*1.2).round(0)
+      else
+        50
+      end
+      if @user.exercise == "everytime" 
+        2200
+      elsif @user.exercise == "Sometimes"
+        2000
+      else
+        1800
+      end
+      if @user.exercise == "everytime"
+        330
+      elsif @user.exercise == "Sometimes"
+        300
+      else
+        270
+      end
+    end
   end
 
   def new
@@ -26,13 +70,21 @@ class EnergysController < ApplicationController
   
   def show
     @user = current_user
-    @days = current_user.days.where(date: @today)
-    @today = Date.today#今日の日付 
-    @energys = current_user.energys.where(date: @today)
+    @energys = current_user.energys.where(date: params[:date])
+    @days = current_user.days.where(date: params[:date])
+    @day = current_user.energys.order(:date).select(:date)
   end
 
   def edit
 
+  end
+
+  def destroy
+    @energy = Energy.find(params[:id])
+    if energy.user_id == current_user.id#もしログインしているユーザーのidと
+      energy.destroy#一致したら消去
+      render :show#一覧ページに戻る
+    end
   end
 
   private
