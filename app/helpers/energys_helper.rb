abf75002ff611a9834c09ef18635955fe366fd3a
junich <GitHub>
2021-12-22 01:05:09 +0900

    energys#index

--- app/helpers/energys_helper.rb
+++ app/helpers/energys_helper.rb
@@ -1,4 +1,6 @@
 module EnergysHelper
+
+  #男性のタンパク質
   def protein_amounts
     if current_user.exercise== "everytime"
       (current_user.weight*1.2).round(0)
@@ -7,11 +9,57 @@ module EnergysHelper
     end
   end
 
-  def protein_amounts#女性
+  #男性のカロリー
+  def kcal_amounts
+    if current_user.exercise == "everytime" && current_user.gender == "man" 
+      2600
+    elsif current_user.exercise == "Sometimes" && current_user.gender == "man"
+      2400
+    elsif current_user.execire == "donot" && current_user.gender == "man"
+      2200
+    end
+  end
+
+  #男性の糖質
+  def my_sugar
+    if current_user.exercise == "everytime" && current_user.gender == "man"
+      390
+    elsif current_user.exercise == "Sometimes" && current_user.gender == "man"
+      360
+    else current_user.exercise == "donot" && current_user.gender== "man"
+      330
+    end
+  end
+
+  #女性のタンパク質
+  def protein_amounts
     if current_user.exercise == "everytime"
       (current_user.weight*1.2).round(0)
     else
       50
     end
   end
+
+  #女性の糖質
+  def my_sugar
+    if current_user.exercise == "everytime" && current_user.gender == "woman"
+      330
+    elsif current_user.exercise == "Sometimes" && current_user.gender == "woman"
+      300
+    else current_user.exercise == "donot" && current_user.gender== "woman"
+      270
+    end
+  end
+
+  #女性のカロリー
+  def kcal_amounts
+    if current_user.exercise == "everytime" && current_user.gender == "woman" 
+      2200
+    elsif current_user.exercise == "Sometimes" && current_user.gender == "woman"
+      2000
+    elsif current_user.execire == "donot" && current_user.gender == "woman"
+      1800
+    end
+  end
+
 end
--- app/models/energy.rb
+++ app/models/energy.rb
@@ -8,38 +8,4 @@ class Energy < ApplicationRecord
   validates :meal
   validates :date
   end
-
-  #男性のカロリー
-  def my_kcal
-    if exercise == "everytime" and gender == "man" 
-      2600-kcal_amounts_sum
-    elsif exercise == "Sometimes" and gender == "man"
-      2400-kcal_amounts_sum
-    elsif execire == "donot" and gender == "man"
-      2200-kcal_amounts_sum
-    end
-  end
-
-  #男性のタンパク質
-  def my_protein
-    if self.exercise == "everytime" and self.gender == "man"
-      (user.weight*1.2).round(0)-protein_amounts
-    elsif self.exercise == "Sometimes" and self.gender == "man"
-      (user.weight*1).round(0)-protein_amounts
-    else self.exercise == "donot" and self.gender== "man"
-      (user.weight*1).round(0)-protein_amounts
-    end
-  end
-
-  #男性の糖質
-  def my_sugar
-    if exercise == "everytime" and gender == "man"
-      390-sugar_amounts_sum
-    elsif exercise == "Sometimes" and gender == "man"
-      360-sugar_amounts_sum
-    else exercise == "donot" and gender== "man"
-      330 -sugar_amounts_sum
-    end
-  end
-
 end
