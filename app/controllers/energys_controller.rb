fb014842489a27c12e0befd9dc442e124676149a
junich <GitHub>
2022-01-08 20:02:25 +0900

    weight-graph

--- app/controllers/energys_controller.rb
+++ app/controllers/energys_controller.rb
@@ -1,44 +1,29 @@
 class EnergysController < ApplicationController
   before_action :authenticate_user!, only: [:index, :new, :create, :list, :edit, :destroy, :edit, :update]
   def index
-    weight_graph_data = current_user.days
+    # weight_graph_datas = current_user.days
+    #この形はよくない（何日前って引数を渡すようにする）
+    #パラメーターで引数を渡してdateの中身を変えるようにする（デフォルトをアクションのアクションにする）
     case params[:graph_sort]
-      when "1"
-        @month_graph_data = weight_graph_data.group_by_day(:date, last: 30).average(:weight)
-      when "2"
-        @half_year_graph_data =  weight_graph_data.group_by_day(:date, last: 180).average(:weight)
-      when "3"
-        @year_graph_data = weight_graph_data.group_by_day(:date, last: 360).average(:weight)
-      when "4"
-        @all_graph_data = weight_graph_data.group_by_day(:date).average(:weight)
+      when "month"
+        month_datas = current_user.days.where(date: Time.current.ago(1.month).beginning_of_day..Time.zone.now.end_of_day)
+        @month_graph = month_datas.map{|n| [n.date, n.weight]}
+      when "half_year"
+        half_year_datas = current_user.days.where(date: Time.current.ago(6.month).beginning_of_day..Time.zone.now.end_of_day)
+        @half_year_graph = half_year_datas.map{|n| [n.date, n.weight]} 
+      when "year"
+        year_datas = current_user.days.where(date: Time.current.ago(1.years).beginning_of_day..Time.zone.now.end_of_day)
+        @year_graph = year_datas.map{|n| [n.date, n.weight]}
+      when "all"
+        all_datas = current_user.days.where("date <= ?", Time.now)
+        @all_graph = all_datas.map{|n| [n.date, n.weight]}
       else
-      # binding.pry
-      #この形にしたい
-    #  @week_graph_data = [["2021-12-24", 55.0] ,["2021-12-26", 66.0] ,["2021-12-31", 54.8]]
-     @week_graph_data = [[Date.today, 55.0] ,[Date.yesterday, 66.0] ,["2021-12-31", 54.8]] 
-        # @week_graph_data = [["Fri, 31 Dec 2021".to_i, 55.0] ["Sat, 01 Jan 2022".to_i, 66.0] ["Tue, 04 Jan 2022".to_i, 54.8]]
-        @week_graph_data = current_user.days.where(date: 1.week.ago.beginning_of_day..Time.zone.now.end_of_day)
-
-
-
-
-
-
-
-
-
-
-
-
-        #ログインしてるユーザーの情報に紐づいたDayモデルの1週間前までのデータをとってきて日付に対応してる体重を配列として括る(１週間前をもっと細分化する)この形をどうやったら出来るかを考える
-        # graph_date[0][:date]
-        # ASC_date = graph_date.sort_by{|data|data.date}
-        # @week_graph_data = weight_graph_data.group(:date).sum(:weight)
-        # @all_graph_data = weight_graph_data.group_by_day(:date, last: 7).average(:weight)
+        week_datas = current_user.days.where(date: 1.week.ago.beginning_of_day..Time.zone.now.end_of_day)
+        @week_graph = week_datas.map{|n| [n.date, n.weight]}
     end
-    #目標体重を計算
+    #目標体重を計算してメイン画面で表示
     @goal_weight =  (current_user.weight*0.95).round(2)
-    #ログインしているユーザーの今日の日付を全件取得（配列）
+
     energys = current_user.energys.where(date: Date.today)
     #メイン画面で適性を超えてるのかを計算して表示する
     @protein_amounts_sum = energys.pluck(:protein).sum
@@ -47,12 +32,11 @@ class EnergysController < ApplicationController
   end
 
   def new
-    @energy = Energy.new#Energyモデルのインスタンスを作る
+    @energy = Energy.new
   end
 
   def create
     if date_judgment
-      #ストロングパラメータを渡してインスタンスを作ってインスタンス変数に代入
       @energy = current_user.energys.build(energy_params)
       date = Date.new energy_params["date(1i)"].to_i, energy_params["date(2i)"].to_i,energy_params["date(3i)"].to_i
       #登録する食事が既にDBにあるかを確認する
