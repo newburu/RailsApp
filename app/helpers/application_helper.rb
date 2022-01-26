b8f2e67c5ae91c4195f13e37c0b74cabc6c487d1
junichmiyahara <junich0911@gmail.com>
2022-01-26 12:38:10 +0900

    password-update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
     # エラーメッセージ用のHTMLを生成
     messages = resource.errors.full_messages_for(:email).join
     html += <<-EOF
-    <div class="error_field">
+    <div class="error-form">
       <p class="error_msg">#{messages}</p>
     </div>
     EOF
@@ -21,9 +21,9 @@ module ApplicationHelper
     messages1 = resource.errors.full_messages_for(:password).join
     messages2 = resource.errors.full_messages_for(:password_confirmation).join
     html += <<-EOF
-    <div class="error_field">
-      <p class="error_msg">#{messages1}</p>
-      <p class="error_msg">#{messages2}</p>
+    <div class="error-form">
+      <p>#{messages1}</p>
+      <p>#{messages2}</p>
     </div>
     EOF
     html.html_safe
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable
+        :recoverable, :rememberable
   has_many :energys
   has_many :days
   enum gender: {man: 0, woman: 1}
@@ -10,13 +10,14 @@ class User < ApplicationRecord
 
   VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,12}\z/
   validates :password, on: :create, presence: true, confirmation: true, format: { with: VALID_PASSWORD_REGEX, message: "は半角6~12文字英大文字・小文字・数字それぞれ１文字以上含む必要があります"}
+  validates :password, on: :update, presence: true, confirmation: true, format: { with: VALID_PASSWORD_REGEX, message: "は半角6~12文字英大文字・小文字・数字それぞれ１文字以上含む必要があります"}, allow_nil: true
   validates :email, on: :create, presence: true, uniqueness: true
   #on: でアクション指定したいけどcreateしか使えないそれだと新規登録の時にエラーメッセージが増える
-  with_options on: :confirm do
-    validates_presence_of :name, length: {maximum:6}
-    validates_presence_of :gender
-    validates_presence_of :weight
-    validates_presence_of :height
-    validates_presence_of :exercise
-  end
+  # with_options on: :create do
+  #   validates_presence_of :name, length: {maximum:6}
+  #   validates_presence_of :gender
+  #   validates_presence_of :weight
+  #   validates_presence_of :height
+  #   validates_presence_of :exercise
+  # end
 end
