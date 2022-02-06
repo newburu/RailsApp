a7def2b4c7b1bb2a6502d7ab2b6a83e4c8dfcca6
junichmiyahara <junich0911@gmail.com>
2022-02-06 14:07:30 +0900

    date_field

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -7,22 +7,17 @@ class DaysController < ApplicationController
   end
 
   def create
-    if date_judgment
-      date = Date.new day_params["date(1i)"].to_i, day_params["date(2i)"].to_i, day_params["date(3i)"].to_i
-      @day = current_user.days.build(day_params)
-      if current_user.days.exists?(date: date)
-        flash.now[:alert] = "既に登録されています"
-        render :new
-      elsif date > Date.today
-        flash.now[:alert] = "明日以降の分は登録出来ません"
-        render :new
-      else
-        @day.save
-        flash[:notice] = '今日の体重を登録しました'
-        redirect_to energys_path
-      end
+    @day = current_user.days.build(day_params)
+    if current_user.days.exists?(date: day_params[:date])
+      flash.now[:alert] = "既に登録されています"
+      render :new
+    elsif Date.parse(day_params[:date]) > Date.today
+      flash.now[:alert] = "明日以降の分は登録出来ません"
+      render :new
     else
-      redirect_to new_day_path, alert: '無効な日付です'
+      @day.save
+      flash[:notice] = '今日の体重を登録しました'
+      redirect_to energys_path
     end
   end
 
@@ -32,22 +27,16 @@ class DaysController < ApplicationController
 
   def update
     @day = Day.find(params[:id])
-    if date_judgment
-      @date = Date.new day_params["date(1i)"].to_i, day_params["date(2i)"].to_i,day_params["date(3i)"].to_i
-      if @date > Date.today
-        flash.now[:alert] = "明日以降の分は登録出来ません"
-        render :edit
-      #編集からも同じ日のを複数登録するのを防ぐ
-      elsif current_user.days.where(date: @date).count > 0 && @day.date != @date
-        redirect_to edit_day_path(@day.id), alert: '既に登録されています'
-      else
-        @day.update(day_params)
-        flash[:notice] = "更新しました"
-        redirect_to controller: 'energys', action: 'list', "date(1i)": params[:day]["date(1i)"], "date(2i)": params[:day]["date(2i)"], "date(3i)": params[:day]["date(3i)"]
-      end
+    if Date.parse(day_params[:date]) > Date.today
+      flash.now[:alert] = "明日以降の分は登録出来ません"
+      render :edit
+    #編集で同じ日の体重を更新できるようにした
+    elsif current_user.days.where(date: day_params[:date]).count > 0 && @day.date != params[:day][:date].to_date
+      redirect_to edit_day_path(@day.id), alert: '既に登録されています'
     else
-      flash[:alert] = "選択された日付は無効な日付です"
-      redirect_to edit_day_path
+      @day.update(day_params)
+      flash[:notice] = "更新しました"
+      redirect_to controller: "energys", action: "list", date: day_params[:date]
     end
   end
 
@@ -56,18 +45,12 @@ class DaysController < ApplicationController
     if @day.user_id == current_user.id
       @day.destroy
       flash[:notice] = "削除しました"
-      redirect_to controller: 'energys', action: 'list', "date(1i)": @day.date.year, "date(2i)": @day.date.month, "date(3i)": @day.date.day
+      redirect_to controller: "energys", action: "list", date: @day.date
     end
   end
 
   private
     def day_params
       params.require(:day).permit(:weight, :date)
-    end
-
-    def date_judgment
-      #無効な日付を入力された時にエラーが出ないように使う
-      date = day_params["date(1i)"].to_i, day_params["date(2i)"].to_i, day_params["date(3i)"].to_i
-      Date.valid_date?(date[0], date[1], date[2])
     end
 end
